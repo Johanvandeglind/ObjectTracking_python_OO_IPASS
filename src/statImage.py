00e1dd31d74b6c4a7f1f9c4b7bb013ecdeccc80a
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-06-11 12:16:19 +0200

    Made class statImage

---
+++ src/statImage.py
@@ -0,0 +1,10 @@
+from src import Image
+class StaticImage(Image.Image):
+    sourcePath = 0
+
+    def __init__(self, sourcePath:int,name:str):
+        self.name = name
+        self.sourcePath = sourcePath
+
+    def get_SourcePath(self):
+        return self.sourcePath
