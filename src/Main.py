8313e60afaca8f54f4100c6c0ead1239696bf7c4
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-06-22 12:21:42 +0200

    Using local opencv as app

--- src/Main.py
+++ src/Main.py
@@ -5,8 +5,8 @@ from src.statImage import StaticImage
 
 class Main:
     cam_image = StaticImage('Recources/topview2.jpg', 'cam_img')
-    robo_dk_image = StaticImage('Recources/cameraSideMount v5_v12.png', 'robodk_img')
+    robo_dk_image = StaticImage('Recources/cameraSideMount v5_v11.png', 'robodk_img')
     live_image = LiveImage(1, 'liveImage')
     imgprocessor = ImageProcessor()
     imgprocessor.show_and_save_image(cam_image.get_SourcePath(), robo_dk_image.get_SourcePath(),
-                                     'combined_image.png', 25, True, live_image.get_CameraNumber())
+                                     'combined_image.png', 25, False, live_image.get_CameraNumber())
