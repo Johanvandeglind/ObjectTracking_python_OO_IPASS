e48536f96a4b899616f2b321d1306d8508951edd
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-06-22 14:42:24 +0200

    added docstring to the App class.

--- src/Application/App.py
+++ src/Application/App.py
@@ -3,6 +3,9 @@ from src.imageProcessing import Image
 
 
 class App():
+    """
+    This class generates the application/window where the image is displayed
+    """
     def show_Image(self, image: Image.Image):
         # if cv2.getWindowProperty("Output")>0:
         #     cv2.clo
