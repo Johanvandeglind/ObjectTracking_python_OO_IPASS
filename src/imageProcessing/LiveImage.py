f0c5ecc768867fa4a08e86f6b4415dddd03f3567
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-06-22 14:41:28 +0200

    added docstring to the LiveImage class.

--- src/imageProcessing/LiveImage.py
+++ src/imageProcessing/LiveImage.py
@@ -2,6 +2,11 @@ from src.imageProcessing import Image
 
 
 class LiveImage(Image.Image):
+    """
+    This class is a extantion of the class Image
+    This class adds the parameter Cameranumber to the object Image.
+    This because the LiveImage does not have a sourcepath.
+    """
     CameraNumber = 0
 
     def __init__(self, CameraNumber:int,name:str):
